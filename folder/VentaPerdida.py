513e7213b0bede1eb05e22aa9ef3e1e17ae93d69
Dmels <136374991+Edwinale20@users.noreply.github.com>
2024-08-01 00:05:41 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -393,6 +393,29 @@ def plot_venta_perdida_con_tendencia(data, view):
     )
     return fig
 
+# Function to plot top 10 artículos con mayor venta perdida
+def plot_articulos_venta_perdida(data):
+    if 'DESC_ARTICULO' not in data.columns:
+        st.warning("La columna 'DESC_ARTICULO' no está en los datos.")
+        return go.Figure()  # Retorna una figura vacía si la columna no está presente
+
+    fig = go.Figure()
+    grouped_data = data.groupby('DESC_ARTICULO')['VENTA_PERDIDA_PESOS'].sum().reset_index()
+    grouped_data = grouped_data.sort_values(by='VENTA_PERDIDA_PESOS', ascending=False).head(10)
+    fig.add_trace(go.Bar(
+        x=grouped_data['DESC_ARTICULO'], 
+        y=grouped_data['VENTA_PERDIDA_PESOS'], 
+        marker_color='rgb(55, 83, 109)'
+    ))
+    fig.update_layout(
+        title='Top 10 Artículos con mayor Venta Perdida',
+        xaxis_title='Artículo',
+        yaxis_title='Venta Perdida (Pesos)',
+        yaxis=dict(tickformat="$,d")
+    )
+    return fig
+
+
 # Function to plot venta perdida por proveedor
 def plot_venta_perdida_proveedor(data, selected_proveedor=None):
     grouped_data = data.groupby('PROVEEDOR')['VENTA_PERDIDA_PESOS'].sum().reset_index()
