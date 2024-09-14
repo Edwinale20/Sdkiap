7b98557b3ce6d5692aaaf6f2ef6df9b098fd3f7f
Dmels <136374991+Edwinale20@users.noreply.github.com>
2024-09-14 12:13:38 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -59,6 +59,7 @@ MASTER = load_file(master_github_url, 'excel')
 
 
 
+
 st.set_page_config(page_title="Reporte de Venta Pérdida Cigarros y RRPS", page_icon="🚬", layout="wide", initial_sidebar_state="expanded")
 st.title("📊 Reporte de Venta Perdida Cigarros y RRPS 🚬")
 
