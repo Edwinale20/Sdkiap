8ff6791495d32f634822e58b8abff793ec3d72b0
Dmels <136374991+Edwinale20@users.noreply.github.com>
2024-09-17 14:17:55 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -10,7 +10,10 @@ import plotly.io as pio
 
 st.set_page_config(page_title="Reporte de Venta Pérdida Cigarros y RRPS", page_icon="🚬", layout="wide", initial_sidebar_state="expanded")
 st.title("📊 Reporte de Venta Perdida Cigarros y RRPS 🚬")
-st.markdown("Datos a partir del 31 de julio del 2024")
+st.markdown("Datos a partir del 31 de julio del 2024, A partir de la semana 34, los datos son venta pérdida y venta son de toda la semana (lunes-domingo)")
+
+
+
 st.markdown("A partir de la semana 34, los datos son venta pérdida y venta son de toda la semana (lunes-domingo)")
 
 # Función para obtener la lista de archivos en una carpeta de GitHub con URL raw
