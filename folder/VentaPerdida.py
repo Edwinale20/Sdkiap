6ad5e2f25175db12db92fd56302c53c5fac817ec
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-03-09 19:08:19 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -692,7 +692,7 @@ def graficar_top_venta_perdida_en_dinero(df_venta_filtrada, df_venta_perdida_fil
         x='Semana Contable', 
         y='VENTA_PERDIDA_PESOS', 
         color='DESCRIPCIÓN',  # Usamos DESCRIPCIÓN en lugar de ARTICULO
-        color_discrete_sequence=px.colors.qualitative.Prism,
+        color_discrete_sequence=['#264653', '#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', '#1D3557', '#457B9D', '#A8DADC']
         text='VENTA_PERDIDA_PESOS',
         title='Top 10 Artículos con Mayor Venta Perdida (En Pesos)',
         labels={'VENTA_PERDIDA_PESOS': 'Venta Perdida en Pesos', 'DESCRIPCIÓN': 'Descripción del Artículo'},
