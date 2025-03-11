7f9aecd7f483b54c89a67f2e5a6578ca05e98b97
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-03-11 17:19:43 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -692,7 +692,11 @@ def graficar_top_venta_perdida_en_dinero(df_venta_filtrada, df_venta_perdida_fil
         x='Semana Contable', 
         y='VENTA_PERDIDA_PESOS', 
         color='DESCRIPCIÓN',  # Usamos DESCRIPCIÓN en lugar de ARTICULO
-        color_discrete_sequence = ['#0D4715', '#B8001F', '#FFB22C', '#0D4715', '#E9762B', '#F9DBBA', '#5B99C2', '#1A4870'],
+        #color_discrete_sequence = ['#0D4715', '#B8001F', '#FFB22C', '#0D4715', '#E9762B', '#F9DBBA', '#5B99C2', '#1A4870'],
+        custom_colors = ['#00712D', '#FF9800', '#000080', '#FFB347', '#33A85C', '#FF6347', '#000000', '#FFD700', 
+                                '#66C88B', '#FF4500', '#FFCC66', '#008080', '#CD5C5C', '#FF7F50', '#006400', '#FFA07A', 
+                                '#8B0000', '#FFDEAD', '#ADFF2F', '#2F4F4F'
+                                ]
         text='VENTA_PERDIDA_PESOS',
         title='Top 10 Artículos con Mayor Venta Perdida (En Pesos)',
         labels={'VENTA_PERDIDA_PESOS': 'Venta Perdida en Pesos', 'DESCRIPCIÓN': 'Descripción del Artículo'},
