95e23f80c94f3b3ef292898efddc582874a79fba
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-03-14 12:36:40 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -335,7 +335,7 @@ def graficar_porcentaje_venta_perdida_por_semana(df_venta_filtrada, df_venta_per
 
     fig.update_traces(
     textposition="top left",
-    textfont=dict(size=13.8)  # Ajusta el valor de size según tus preferencias
+    textfont=dict(size=18)  # Ajusta el valor de size según tus preferencias
     )
 
     return fig
@@ -691,8 +691,8 @@ def graficar_top_venta_perdida_en_dinero(df_venta_filtrada, df_venta_perdida_fil
         x='Semana Contable', 
         y='VENTA_PERDIDA_PESOS', 
         color='DESCRIPCIÓN',  # Usamos DESCRIPCIÓN en lugar de ARTICULO
-        color_discrete_sequence = ['#00712D', '#FF9800', '#9694FF', '#FFB347', '#33A85C', '#1230AE', '#8D0B41', '#FFD700', 
-         '#66C88B', '#FF4500', '#FFA07A', '#8B0000', '#FFDEAD', '#ADFF2F', '#2F4F4F', "#4D55CC", "#640D5F", "#16404D"],
+        color_discrete_sequence = ['#007074', '#FF9800', '#9694FF', '#FFB347', '#33A85C', '#1230AE', '#8D0B41', '#FFD700', 
+         '#66C88B', '#EB5B00', '#FFA07A', '#B80000', '#FFDEAD', '#ADFF2F', '#2F4F4F', "#4D55CC", "#640D5F", "#16404D"],
         text='VENTA_PERDIDA_PESOS',
         title='Top 10 Artículos con Mayor Venta Perdida (En Pesos)',
         labels={'VENTA_PERDIDA_PESOS': 'Venta Perdida en Pesos', 'DESCRIPCIÓN': 'Descripción del Artículo'},
