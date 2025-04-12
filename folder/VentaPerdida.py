fc96f40f2fb0b401ee6f50994f3f0a8d9a01e66e
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-04-12 12:40:20 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -655,7 +655,7 @@ def graficar_venta_perdida_por_plaza(df_venta_perdida_filtrada, df_venta_filtrad
         yaxis_title='% Venta Perdida',
         hovermode='closest',
         title_font=dict(size=20),
-        template="colors2",
+        #template="colors2",
         showlegend=True
     )
 
