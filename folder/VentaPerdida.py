aef199b302d1257849c3f5badb37e55801f4b6de
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-04-12 11:55:28 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -631,7 +631,7 @@ def graficar_venta_perdida_por_plaza(df_venta_perdida_filtrada, df_venta_filtrad
             mode='lines+markers+text',
             text=df_plaza['% Venta Perdida'].apply(lambda x: f'{x:.1f}%'),
             textposition='top right',
-            name=plaza
+            name=plaza,
             hovertemplate=
                 '<b>Semana:</b> %{x}<br>'+
                 '<b>% Venta Perdida:</b> %{y:.1f}%<br>'+
