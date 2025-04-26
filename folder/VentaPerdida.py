c21373c6322b46a33d64e6571f2c9cfe0a41681a
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-04-26 12:24:06 -0600

    Update VentaPerdida.py

--- folder/VentaPerdida.py
+++ folder/VentaPerdida.py
@@ -251,7 +251,7 @@ else:
     print("La columna 'Venta Neta Total' no existe en el DataFrame.")
 
 #---------------------------------------------------------------------
-st.sidebar.image("el-logo.png", use_column_width=True)
+st.sidebar.image("https://github.com/Edwinale20/Sdkiap/blob/main/folder/el-logo.png", use_column_width=True)
 st.sidebar.title("Filtros 🔠")
 
 
